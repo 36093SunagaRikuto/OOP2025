de8ba2e918a47c8c45320de4d2d9f2e1a5ea8ff4
infosys <infosys@C4215>
2025-04-21 09:57:53 +0900

    プロジェクト ファイルを追加します。

---
+++ Chapter01/ProductSample/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ProductSample{
+    internal class Program{
+        static void Main(string[] args) {
+
+            Product karinto = new Product(123,"かりんとう", 180);
+            Product daifuku = new Product(124,"大福", 100);
+
+
+
+
+            //税抜きの価格を表示
+            Console.WriteLine(daifuku.Name + "の税抜き価格は" + daifuku.Price + "円です");
+
+            //消費税額の表示
+            Console.WriteLine(daifuku.Name + "の消費税額は" + daifuku.ToString() + "円です");
+
+            //税込みの価格の表示
+            Console.WriteLine(karinto.Name + "の税込み価格は" + karinto.GetPriceIncludingTax() + "円です");
+
+        }
+    }
+}
---
+++ Chapter02/DistanceConverter/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics.Metrics;
+using System.Numerics;
+
+namespace DistanceConverter {
+    internal class Program {
+        static void Main(string[] args) {
+
+            int start = int.Parse(args[1]);
+            int end = int.Parse(args[2]);
+
+            if (args.Length > 0 && args[0] == "-tom") {
+
+                //フィートからメートルへの対応表を出力
+                PirntFeetToMeterList(start, end);
+
+
+            } else {
+
+                //メートルからフィートへの対応表を出力
+                PrintMeterToFeetList(start, end);
+
+
+            }
+            Console.WriteLine("\b  ");
+            Nakami(args);
+            Console.WriteLine("\n");
+        }
+
+        private static void PrintMeterToFeetList(int start, int end) {
+            for (int meter = start; meter <= end; meter++) {
+                double m1 = FeetToMeter(meter);
+                Console.WriteLine($"{meter}m = {m1:0.0000}ft");
+            }
+        }
+
+        private static void PirntFeetToMeterList(int start, int end) {
+            for (int meter = start; meter <= end; meter++) {
+                double m1 = FeetToMeter(meter);
+                Console.WriteLine($"{meter}ft = {m1:0.0000}m");
+
+            }
+        }
+
+        private static void Nakami(string[] args) {
+            for (int i = 0; i < args.Length; i++) {
+                Console.Write($"{args[i]}   ");
+            }       
+        }
+
+
+        static double FeetToMeter(int feet) {
+            return feet * 0.3048; ;
+        }
+
+        static double MeterToFeet(int meter) {
+            return meter / 0.3048;
+        }
+
+
+    }
+}
