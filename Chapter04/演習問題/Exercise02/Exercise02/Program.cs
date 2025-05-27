4b8c20dbc6897b3fc0dddae2ee5a6fb4778ccdc3
36093SunagaRikuto <oibc36093@ojs.ac.jp>
2025-05-27 09:31:38 +0900

    Chapter04完成

--- Chapter04/演習問題/Exercise02/Exercise02/Program.cs
+++ Chapter04/演習問題/Exercise02/Exercise02/Program.cs
@@ -13,25 +13,63 @@ namespace Exercise02 {
         }
 
         private static void Exercise1() {
-            int num = int.Parse(Console.ReadLine());
-            if (num < 0)
-                Console.WriteLine("あ");
-                Console.WriteLine(num);
-                
-            else if (num < 100)
-                Console.WriteLine(num * 2);
-            else if (num < 500)
-                Console.WriteLine(num * 3);
-            else
-                Console.WriteLine(num);
+            //int num = int.Parse(Console.ReadLine());
+            var line = Console.ReadLine();
+            if (int.TryParse(line, out var num)) {
+                if (num < 0)
+                    Console.WriteLine(num);
+
+                else if (num < 100)
+                    Console.WriteLine(num * 2);
+                else if (num < 500)
+                    Console.WriteLine(num * 3);
+                else
+                    Console.WriteLine(num);
+            } else {
+                Console.WriteLine("入力に誤りがあります。");
+            }
         }
 
         private static void Exercise2() {
-            throw new NotImplementedException();
+            var num = Console.ReadLine();
+            if (int.TryParse(num, out var line)) {
+                int nums = int.Parse(num);
+                switch (nums) {
+                    case < 0:
+                        Console.WriteLine(nums);
+                        break;
+                    case < 100:
+                        Console.WriteLine(nums * 2);
+                        break;
+                    case < 500:
+                        Console.WriteLine(nums * 3);
+                        break;
+                    case >= 500:
+                        Console.WriteLine(num);
+                        break;
+                    default:
+                        Console.WriteLine("入力に誤りがあります。");
+                        break;
+
+                }
+            } else {
+                Console.WriteLine("入力に誤りがあります");
+            }
         }
 
         private static void Exercise3() {
-            throw new NotImplementedException();
+            var line = Console.ReadLine();
+            if (int.TryParse(line, out var num)) {
+                var text = num switch {
+                    < 0 => num,
+                    < 100 => num * 2,
+                    < 500 => num * 3,
+                    _ => num
+                };
+                Console.WriteLine(text);
+            } else {
+                Console.WriteLine("入力に誤りがあります");
+            }
         }
     }
 }
