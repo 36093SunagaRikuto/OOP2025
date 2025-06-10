7edd19a878eac25574618a67418f6940bf7781b5
36093SunagaRikuto <oibc36093@ojs.ac.jp>
2025-06-10 12:09:21 +0900

    Chapter05演習問題

---
+++ Chapter05/Secton02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Secton02 {
+    internal class Program {
+        static void Main(string[] args) {
+
+
+            var appVer1 = new AppVersion(5, 1);
+            var appVer2 = new AppVersion(5, 1);
+
+            Console.WriteLine(appVer1);
+            if(appVer1 == appVer2) {
+                Console.WriteLine("ひとし");
+            } else {
+                Console.WriteLine("ひとしくなさし");
+            }
+
+        }
+    }
+
+    //プライマリーコンストラクタを使ったクラス定義
+    public record AppVersion(int Major, int Minor, int Build = 0, int Revision = 0) {
+        
+
+
+
+
+
+
+
+        public override string ToString() =>
+            $"{Major}.{Minor}.{Build}.{Revision}";
+    }
+
+}
---
+++ Chapter05/演習問題/Exercise02/Program.cs
@@ -0,0 +1,68 @@
+﻿using Exercise01;
+
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            // 5.2.1
+            var ymCollection = new YearMonth[] {
+                new YearMonth(1980, 1),
+                new YearMonth(1990, 4),
+                new YearMonth(2025, 7),
+                new YearMonth(2010, 9),
+                new YearMonth(2024, 12),
+            };
+
+            Console.WriteLine("5.2.2");
+            Exercise2(ymCollection);
+
+            Console.WriteLine("5.2.4");
+            Exercise4(ymCollection);
+
+
+            Console.WriteLine("5.2.5");
+            Exercise5(ymCollection);
+        }
+
+        private static void Exercise2(YearMonth[] ymCollection) {
+
+            ymCollection.ToList().ForEach(x => Console.WriteLine(x));
+
+        }
+
+        private static void Exercise4(YearMonth[] ymCollection) {
+
+            /*if(FirstFind21(ymCollection) is null) {
+                Console.WriteLine("21世紀のデータはありません");
+            } else {
+                Console.WriteLine(FirstFind21(ymCollection));
+            }*/
+
+            Console.WriteLine(FirstFind21(ymCollection).ToString() ?? "21世紀のデータはありません");
+
+        }
+
+        private static void Exercise5(YearMonth[] ymCollection) {
+
+            var next = ymCollection.Select(x => x.AddOneMonth()).OrderBy(n => n.Month).OrderBy(n => n.Year);
+            //var sort = next.OrderBy(n =>n.Month).OrderBy(n => n.Year);
+
+            ;
+            foreach(var chi in next) {
+                
+                Console.WriteLine(chi);
+            }
+        }
+
+        
+        //5.2.3
+        private static YearMonth? FirstFind21(YearMonth[] ymCollection) {
+            foreach(var first in ymCollection) {
+                if (first.Is21Century) {
+                    return first;
+                }   
+            }
+            return null;
+        }
+    }
+}
