2be582cf44a2ba42a3d48290ec98d1aa5fb7295b
36093SunagaRikuto <oibc36093@ojs.ac.jp>
2025-06-17 09:45:34 +0900

    問題8-2年度

--- Chapter08/PrefCapitalLocationSystem/Program.cs
+++ Chapter08/PrefCapitalLocationSystem/Program.cs
@@ -1,8 +1,21 @@
 ﻿using System.Security.Cryptography;
+using static PrefCapitalLocationSystem.Program;
 
 namespace PrefCapitalLocationSystem {
     internal class Program {
         static private Dictionary<string, string> prefOfficeDict = new Dictionary<string, string>();
+
+        //列挙型の定義
+        public　enum SelectMenu { 
+            Alldisp = 1,
+            Search = 2,
+            Exit = 9
+        }
+
+
+
+
+
 
         static void Main(string[] args) {
             String? pref, prefCaptalLocation;
@@ -50,7 +63,25 @@ namespace PrefCapitalLocationSystem {
 
                 Boolean endFlag = false;    //終了フラグ（無限ループを抜け出す用）
                 while (!endFlag) {
-                    switch (menuDisp()) {
+
+                switch (menuDisp()) {
+                    case SelectMenu.Alldisp:                        //一覧出力処理
+                        allDisp();
+                        break;
+
+
+                    case SelectMenu.Search://検索処理
+                        searchPrefCaptalLocation();
+                        break;
+
+
+                    case SelectMenu.Exit://無限ループを抜ける
+                        endFlag = true;
+                        break;
+
+                
+
+                        /*switch (menuDisp()) {
                         case "1":                        //一覧出力処理
                             allDisp();
                             break;
@@ -65,20 +96,24 @@ namespace PrefCapitalLocationSystem {
                             endFlag = true;
                             break;
 
-
-                    
+                        case null:
+                            break;
+                        */
                     }
                 }
         }
 
         //メニュー表示
-        private static string? menuDisp() {
+        private static SelectMenu menuDisp() {
             Console.WriteLine("\n**** メニュー ****");
             Console.WriteLine("1：一覧表示");
             Console.WriteLine("2：検索");
             Console.WriteLine("9：終了");
             Console.Write(">");
-            var menuSelect = Console.ReadLine();
+
+            var menuSelect = (SelectMenu)int.Parse(Console.ReadLine());
+
+
             if(prefOfficeDict is null) {
                 Console.WriteLine("表示するものはありません");
             }
