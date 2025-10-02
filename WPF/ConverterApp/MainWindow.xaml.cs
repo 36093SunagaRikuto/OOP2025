dc32ed57ce3b9c0d529e6a6b17e7ec57ed884c53
36093SunagaRikuto <oibc36093@ojs.ac.jp>
2025-10-02 14:59:12 +0900

    InotifProperttyChangedの場合

--- WPF/ConverterApp/MainWindow.xaml.cs
+++ WPF/ConverterApp/MainWindow.xaml.cs
@@ -18,11 +18,40 @@ namespace ConverterApp
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+    public partial class MainWindow : Window{
+
+        Dictionary<string,double> tanni = new Dictionary<string, double> {
+            {"m", 1},
+            {"km", 1000},
+            {"cm", 0.01},
+            {"mm", 0.001},
+            {"mile", 1609.34},
+            {"yard", 0.9144},
+            {"foot", 0.3048},
+            {"inch", 0.0254}
+        };
+
+        public MainWindow(){
             InitializeComponent();
+            MetricUnit.ItemsSource = tanni.Keys;
+            ImperialUnit.ItemsSource = tanni.Keys;
+        }
+
+        private void ImperialUnitToMetoric_Click(object sender, RoutedEventArgs e) {
+
+
+            
+            if(int.TryParse(ImperialValue.Text, out int result)){
+                MetricValue.Text = (result / 1).ToString();
+            }
+           
+        }
+
+        private void MetricToImperialUnit_Click(object sender, RoutedEventArgs e) {
+            
+            if(int.TryParse(MetricValue.Text, out int result)) {
+                ImperialValue.Text = (result * 1).ToString();
+            }
         }
     }
 }
---
+++ WPF/HelloWorld/ViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld{
+    class ViewModel : BindableBase{
+        public ViewModel() {
+            ChangeMessageCommand = new DelegateCommand(
+                () => GreetingMessage = "Bye-bye world");
+        }
+        private string _greetingMessage = "Hello, World!";
+
+        public string GreetingMessage {
+            get => _greetingMessage;
+            set => SetProperty(ref _greetingMessage, value);
+            
+        }
+
+        public DelegateCommand ChangeMessageCommand { get; }
+
+       
+    }
+}
