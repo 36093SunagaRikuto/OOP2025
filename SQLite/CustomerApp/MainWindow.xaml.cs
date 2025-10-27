58175034e20297f85edcf21e1b606ca524a2eee8
36093SunagaRikuto <oibc36093@ojs.ac.jp>
2025-10-27 14:29:58 +0900

    Prism対応

--- SQLite/CustomerApp/MainWindow.xaml.cs
+++ SQLite/CustomerApp/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using CustomerApp.Data;
 using Microsoft.Win32;
 using SQLite;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Text;
 using System.Windows;
@@ -20,18 +21,22 @@ namespace CustomerApp;
 /// </summary>
 public partial class MainWindow : Window
 {
+    OpenFileDialog openfile = new OpenFileDialog();
+    private ObservableCollection<Customer> _customer = new ObservableCollection<Customer>();
     public MainWindow()
     {
         InitializeComponent();
         ReadDB();
+        
     }
 
     private void save_Click(object sender, RoutedEventArgs e) {
+        MemoryStream ms = new MemoryStream();
         var customer = new Customer {
             Name = Name.Text,
             Phone = Phone.Text,
             Address = Address.Text,
-            //Picture = picture.Source
+            Picture = File.ReadAllBytes(openfile.FileName)
         };
 
         using (var conect = new SQLiteConnection(App.databasePath)) {
@@ -46,8 +51,8 @@ public partial class MainWindow : Window
     }
 
     private void gbt_Click(object sender, RoutedEventArgs e) {
-        OpenFileDialog openfile = new OpenFileDialog();
-
+        
+        
         openfile.Filter = "画像ファイル(*.png;*.jpg;*.jpeg;*bmp) | *.png;*.jpg;*bmp";
         if(openfile.ShowDialog() == true) {
             string filepath = openfile.FileName;
@@ -59,6 +64,7 @@ public partial class MainWindow : Window
                 bit.StreamSource = stream;
                 bit.EndInit();
             }
+           
             picture.Source = bit;
 
         } else {
@@ -67,9 +73,6 @@ public partial class MainWindow : Window
         
     }
 
-    private void imagedelete_Click(object sender, RoutedEventArgs e) {
-        picture.Source = null;
-    }
 
     private void picture_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
         
@@ -107,9 +110,48 @@ public partial class MainWindow : Window
         using (var connection = new SQLiteConnection(App.databasePath)) {
             connection.CreateTable<Customer>();
             viewview.ItemsSource = connection.Table<Customer>().ToList();
-
+            
         }
 
 
+    }
+
+    private void TextBox_DragEnter(object sender, DragEventArgs e) {
+
+    }
+
+    private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e) {
+        var search = _customer.Where(p => p.Name.Contains(Searchtb.Name));
+
+        viewview.ItemsSource = search;
+    }
+
+    private void viewview_SelectionChanged_1(object sender, SelectionChangedEventArgs e) {
+        if (viewview.SelectedItem is null) return; 
+        var item = viewview.SelectedItem as Customer;
+        Name.Text = item.Name;
+        Phone.Text = item.Phone;
+        Address.Text = item.Address;
+
+    }
+
+    private void update_Click(object sender, RoutedEventArgs e) {
+        var unni = viewview.SelectedItem as Customer;
+        if (unni is null) {
+            return;
+        }
+        using (var connection = new SQLiteConnection(App.databasePath)) {
+
+            var customer = new Customer() {
+                Id = unni.Id,
+                Name = Name.Text,
+                Phone = Phone.Text,
+                Picture = unni.Picture
+                
+            };
+            connection.Update(customer);
+
+            ReadDB();
+        }
     }
 }
